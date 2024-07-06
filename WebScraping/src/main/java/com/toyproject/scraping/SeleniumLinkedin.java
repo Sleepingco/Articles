52a8d03948b0a53cbdc783fbf14160e49ad84cd2
Sleepingco <hosuhosuhosu14@gmail.com>
2024-07-07 00:45:01 +0900

    make linked in scrap

--- WebScraping/src/main/java/com/toyproject/scraping/SeleniumLinkedin.java
+++ WebScraping/src/main/java/com/toyproject/scraping/SeleniumLinkedin.java
@@ -2,6 +2,7 @@ package com.toyproject.scraping;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,7 +23,10 @@ import org.springframework.stereotype.Service;
 
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -74,71 +78,79 @@ public class SeleniumLinkedin {
 //		        break; // 더 이상 로드되는 콘텐츠가 없으면 반복 종료
 //		    }
 //		}
-		int idx = 0;
+		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#fie-impression-container > div.feed-shared-update-v2__description-wrapper.mr2 > div > div > span")));
 		try {
 			// 내용이 담겨있는 div 하나
 		    List<WebElement> divs = driver.findElements(By.cssSelector("#fie-impression-container"));
+		    Collections.reverse(divs);
 		    if (divs.isEmpty()) {
 		        System.out.println("No divs found.");
 		    } else {
 		    	// 각요소 추출 글쓴이, 내용 등등..
 		        for (WebElement div : divs) {
 		        	WebElement author = div.findElement(By.cssSelector("#fie-impression-container > div.relative > div.update-components-actor.display-flex.update-components-actor--with-control-menu > div > div > a.app-aware-link.update-components-actor__meta-link > span.update-components-actor__title > span.update-components-actor__name.hoverable-link-text.t-14.t-bold.t-black > span > span:nth-child(1)"));
-		        	WebElement contentText = null;
-		        	String contentLinkText = null;
-		        	String htmlLink = null;
-//		        	WebElement link = null;
+		        	String content  = "";
+		        	String originalPage = "";
 		        	try {
 		        		
-		        		contentText = div.findElement(By.cssSelector("#fie-impression-container > div.feed-shared-update-v2__description-wrapper.mr2 > div > div > span > span"));
-		        		
-		        		// 컨텐츠 내부에 링크 박스를 알아내기
-		        		WebElement contentLinkBox = div.findElement(By.cssSelector("#fie-impression-container > article > div"));
-		        		contentLinkText = contentLinkBox.findElement(By.tagName("a")).getAttribute("href");
-		        		
-		        		// 컨턴츠 내부에 링크 알아내기
+		        		// 컨텐츠 와 내부에 링크 알아내기
 		        		String htmlContent = div.findElement(By.cssSelector("#fie-impression-container > div.feed-shared-update-v2__description-wrapper.mr2 > div > div > span > span")).getAttribute("innerHTML");
 		        		Document doc = Jsoup.parseBodyFragment(htmlContent);
-		        		htmlLink = parseHtmlContent(doc);
-		        		System.out.println("doc : "+doc);
-//		        		System.out.println("hmtl text content : "+idx+" "+htmlLink);
-//		        		System.out.println("hmtl text content link box : "+idx+" "+contentLinkText);
-		        		idx++;
+		        		content = doc.text();
+		        		// 컨텐츠 내부에 링크 박스를 알아내기
+		        		content += " "+getLinkBoxSafe(div);
+		        		
+		        		// 원본 url 복사
+		        		try {
+		        			WebElement button = div.findElement(By.cssSelector(".feed-shared-control-menu__trigger.artdeco-button.artdeco-button--tertiary.artdeco-button--muted.artdeco-button--1.artdeco-button--circle.artdeco-dropdown__trigger.artdeco-dropdown__trigger--placement-bottom.ember-view"));
+		                    wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+		        		    WebElement linkShare = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".feed-shared-control-menu__item.option-share-via")));
+		        		    linkShare.click();
+		        		    WebElement shareBox = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.artdeco-toast-item__cta")));
+		        		    originalPage = shareBox.getAttribute("href");
+		                    Thread.sleep(1000);
+		                    WebElement closeBox = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".artdeco-toast-item__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--1.artdeco-button--tertiary.ember-view")));
+		                    closeBox.click();
+		        		} catch (Exception e) {
+		        		    System.out.println("original Link copy fail : " + e.getMessage());
+		        		}
 		        	} catch(NoSuchElementException e) {
-		        		System.out.println("can't find element : "+e);
+		        		System.out.println("can't find element <<<<<<<<<<<<<<<<<<<<: "+e);
 		        	}
 		        	WebElement name = div.findElement(By.cssSelector("#fie-impression-container > div.relative > div.update-components-actor.display-flex.update-components-actor--with-control-menu > div > div > a.app-aware-link.update-components-actor__meta-link > span.update-components-actor__title > span.update-components-actor__name.hoverable-link-text.t-14.t-bold.t-black > span > span:nth-child(1)"));
-
-//		            String authors = author.getText();
-//		            String contents = content.getText();
-//		            String names = name.getText();
-//		            articleDAO.saveLinkedinArticle(authors, contents, names, urljojoldu);
+		            String authors = author.getText();
+		            String nameText = name.getText();
+		            System.out.println("author is :"+authors);
+		            System.out.println("content is : "+content);
+		            System.out.println("name is : "+ nameText);
+		            System.out.println("url is : "+ originalPage);
+//		            articleDAO.saveLinkedinArticle(authors, content, nameText, originalPage);
 		        }
 		    }
 		} catch (Exception e) {
 		    System.err.println("An error occurred: " + e.getMessage());
 		    e.printStackTrace();
 		}
-		driver.quit();
+//		driver.quit();
 	}
 	
-	// 작동안함 체크해야함
-    public static String parseHtmlContent(Document doc) {
-        StringBuilder textWithLinks = new StringBuilder();
-        for (Element element : doc.body().children()) {
-            if (element.tagName().equals("a")) {
-                textWithLinks.append(element.text())
-                             .append(" (")
-                             .append(element.attr("href"))
-                             .append(") ");
-            } else {
-                textWithLinks.append(element.text());
-            }
+
+    
+    // 컨텐츠 내부에 링크 박스를 알아내기
+    private static String getLinkBoxSafe(WebElement div) {
+        try {
+        	WebElement contentLinkBox = div.findElement(By.cssSelector("#fie-impression-container > article > div"));
+            return contentLinkBox.findElement(By.tagName("a")).getAttribute("href");
+        } catch (NoSuchElementException e) {
+            System.out.println(div + " not found: " + e);
+            return "";
         }
-        return textWithLinks.toString();
     }
     
+
     
 }
